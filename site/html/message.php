af6d879f4648e170c5a0c623d82216d6e27d471c
Naludrag <stephanecarvalho81@gmail.com>
2020-09-21 17:10:43 +0200

    Cleaned message.php and added comments

--- site/html/message.php
+++ site/html/message.php
@@ -9,25 +9,22 @@ $file_db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
 $file_db->setAttribute(PDO::ATTR_ERRMODE,
     PDO::ERRMODE_EXCEPTION);
 
+// Check that the form is completed
 if (isset($_POST['recipient']) && isset($_POST['subject']) && isset($_POST['body'])) {
     $stmt = $file_db->prepare("SELECT * FROM Users WHERE id=:id");
     $stmt->execute(['id' => $_SESSION['id']]);
     $user = $stmt->fetch();
+    // Check that the session has a valid id
     if($user != NULL) {
         $users = $file_db->query('SELECT * FROM Users');
-        // Check if the user sent by the form exists in the database
+        // Check if the recipient wrote in the form exists in the database. If the user is unknown the site will do nothing
         foreach ($users as $row) {
             if ($_POST['recipient'] == $row['username']) {
+                // Prepare the date in the right format to write in the database
                 $date = new DateTime();
                 $date = $date->format('d-m-Y H:i:s');
-                $data = [
-                    'subject' => $_POST['subject'],
-                    'body' => $_POST['$body'],
-                    'sender' => $_SESSION['id'],
-                    'recipient' => $row['id'],
-                    'date' => $date
-                ];
-                var_dump($row['id']);
+
+                // Prepare the database request to insert a new message
                 $sql = "INSERT INTO Messages (subject, body, sender, recipient, date) VALUES (:subject, :body, :sender, :recipient, :date)";
                 $stmt2 = $file_db->prepare($sql);
                 $stmt2->bindParam(':subject',$_POST['subject']);
@@ -36,9 +33,6 @@ if (isset($_POST['recipient']) && isset($_POST['subject']) && isset($_POST['body
                 $stmt2->bindParam(':recipient',$row['id']);
                 $stmt2->bindParam(':date',$date);
                 $stmt2->execute();
-                //;
-            } else {
-                echo "User unknown please enter a valid username";
             }
         }
     } else {
