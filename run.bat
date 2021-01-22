9eadb11a4c41ee66ceefbad4e1ee4c1ffcad4b9d
b3cauda <arthur.becaud@heig-vd.ch>
2021-01-22 13:37:42 +0100

    remove unused pentest folder

--- run.bat
+++
@@ -1,4 +0,0 @@
-docker rm -f sti_project
-docker run -ti -v "%cd%/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
-docker exec -u root sti_project service nginx start
-docker exec -u root sti_project service php5-fpm start
